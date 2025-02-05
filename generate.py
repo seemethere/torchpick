8d59f6328e19e4c4276d9abed0bcc0ebee5c6f78
Eli Uriegas <eliuriegas@meta.com>
2025-02-05 09:29:43 -0800

    use platform markers, build regular wheel
    Signed-off-by: Eli Uriegas <eliuriegas@meta.com>

--- generate.py
+++ generate.py
@@ -2,36 +2,20 @@
 
 # /// script
 # requires-python = ">=3.11"
-# dependencies = ["setuptools", "wheel", "build"]
+# dependencies = ["build"]
 # ///
 
 import sys
 import subprocess
-import os
-import pathlib
-from setuptools.command.bdist_wheel import get_platform
-
-ROOT_DIR = pathlib.Path(os.path.realpath(__file__)).parent
 
 
 def build_wheel():
     subprocess.run([sys.executable, "-m", "build"], check=True)
 
 
-def rename_wheel():
-    platform_tag = get_platform(".")
-    torch_version = open(ROOT_DIR / "VERSION").read().strip()
-    # Assuming the wheel file is in the dist directory and has a standard naming convention
-    wheel_file = f"dist/torchpick-{torch_version}-py3-none-any.whl"
-    new_wheel_file = f"dist/torchpick-{torch_version}-py3-none-{platform_tag}.whl"
-    os.rename(wheel_file, new_wheel_file)
-
-
 def main() -> None:
     print("+ Building wheel")
     build_wheel()
-    print("+ Renaming wheel")
-    rename_wheel()
 
 
 if __name__ == "__main__":
--- hooks/generate_metadata.py
+++ hooks/generate_metadata.py
@@ -1,51 +1,50 @@
 import os
 import pathlib
 from hatchling.metadata.plugin.interface import MetadataHookInterface
-from setuptools.command.bdist_wheel import get_platform
 
 ROOT_DIR = pathlib.Path(os.path.realpath(__file__)).parent.parent
 
+LINUX = "platform_system == 'Linux'"
+LINUX_X86 = "platform_system == 'Linux' and platform_machine == 'x86_64'"
+LINUX_OR_WINDOWS = f"{LINUX} or platform_system == 'Windows'"
 
-def generate_torch_dependency(version: str, suffix: str = "") -> str:
-    return f"torch=={version}{'+' if suffix else ''}{suffix}"
+
+def generate_torch_dependency(
+    version: str, suffix: str = "", specifiers: str = ""
+) -> str:
+    return f"torch=={version}{'+' if suffix else ''}{suffix}{';' if specifiers else ''}{specifiers}"
 
 
 class JSONMetaDataHook(MetadataHookInterface):
     def update(self, metadata):
         torch_version = open(ROOT_DIR / "VERSION").read().strip()
-        platform = get_platform(".")
-        platform_dependencies = {
-            "linux_x86_64": {
-                "cpu": [generate_torch_dependency(torch_version, "cpu")],
-                "cpu.cxx11.abi": [
-                    generate_torch_dependency(torch_version, "cpu.cxx11.abi")
-                ],
-                "cuda": [generate_torch_dependency(torch_version, "cu124")],
-                "cuda12.4": [generate_torch_dependency(torch_version, "cu124")],
-                "cuda12.1": [generate_torch_dependency(torch_version, "cu121")],
-                "cuda11.8": [generate_torch_dependency(torch_version, "cu118")],
-                "rocm": [generate_torch_dependency(torch_version, "rocm6.2.4")],
-                "rocm6.2": [generate_torch_dependency(torch_version, "rocm6.2.4")],
-                "rocm6.1": [generate_torch_dependency(torch_version, "rocm6.1")],
-                "xpu": [generate_torch_dependency(torch_version, "xpu")],
-            },
-            "macos": {
-                "cpu": [generate_torch_dependency(torch_version)],
-            },
-            "win_amd64": {
-                "cpu": [generate_torch_dependency(torch_version, "cpu")],
-                "cpu.cxx11.abi": [
-                    generate_torch_dependency(torch_version, "cpu.cxx11.abi")
-                ],
-                "cuda": [generate_torch_dependency(torch_version, "cu124")],
-                "cuda12.4": [generate_torch_dependency(torch_version, "cu124")],
-                "cuda12.1": [generate_torch_dependency(torch_version, "cu121")],
-                "cuda11.8": [generate_torch_dependency(torch_version, "cu118")],
-                "xpu": [generate_torch_dependency(torch_version, "xpu")],
-            },
+        metadata["optional-dependencies"] = {
+            "cpu": [generate_torch_dependency(torch_version, "cpu", LINUX_OR_WINDOWS)],
+            "cpu.cxx11.abi": [
+                generate_torch_dependency(
+                    torch_version, "cpu.cxx11.abi", LINUX_OR_WINDOWS
+                )
+            ],
+            "cuda": [
+                generate_torch_dependency(torch_version, "cu124", LINUX_OR_WINDOWS)
+            ],
+            "cuda12.6": [
+                generate_torch_dependency(torch_version, "cu126", LINUX_OR_WINDOWS)
+            ],
+            "cuda12.4": [
+                generate_torch_dependency(torch_version, "cu124", LINUX_OR_WINDOWS)
+            ],
+            "cuda12.1": [
+                generate_torch_dependency(torch_version, "cu121", LINUX_OR_WINDOWS)
+            ],
+            "cuda11.8": [
+                generate_torch_dependency(torch_version, "cu118", LINUX_OR_WINDOWS)
+            ],
+            "xpu": [generate_torch_dependency(torch_version, "xpu", LINUX_OR_WINDOWS)],
+            "rocm": [generate_torch_dependency(torch_version, "rocm6.2.4", LINUX_X86)],
+            "rocm6.2": [
+                generate_torch_dependency(torch_version, "rocm6.2.4", LINUX_X86)
+            ],
+            "rocm6.1": [generate_torch_dependency(torch_version, "rocm6.1", LINUX_X86)],
         }
-        # special case for macOS
-        if platform.startswith("macos"):
-            platform = "macos"
         metadata["version"] = torch_version
-        metadata["optional-dependencies"] = platform_dependencies.get(platform, [])
